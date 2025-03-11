ee1378e88186ef28c6d1285a4a2e30efc3b4349e
Blair Replogle <UpBra@users.noreply.github.com>
2025-03-11 14:57:04 -0600

    TLKA-0005: Status Testflight Deploy Helper (#5)

---
+++ lib/fastlane/plugin/toolkit_apple/source/status.rb
@@ -0,0 +1,19 @@
+# -------------------------------------------------------------------------
+#
+# Post Status
+# Posts status messages to multiple chat clients (Teams and Slack)
+#
+# -------------------------------------------------------------------------
+
+module Status
+
+	Actions = Fastlane::Actions
+	SharedValues = Actions::SharedValues
+
+	def self.add_testflight_deploy_facts
+		return unless name ||= Actions.lane_context.fetch(SharedValues::TESTFLIGHT_DEPLOY_APP_DISPLAY_NAME)
+		return unless value ||= Actions.lane_context.fetch(SharedValues::TESTFLIGHT_DEPLOY_ITUNES_URL)
+
+		Status.add_fact(name, value)
+	end
+end
